1ce1372c63e201cda308ba0c8e9a272010ef06a5
Ali Mohammad <aligamerhd15@gmail.com>
2026-03-14 00:25:54 +0300

    3.1.0 'changed each role to have its own custom count and added employee creation in seeder'

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,8 @@ use App\Models\User;
 use App\Models\Category;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Division;
+use App\Models\Employee;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,19 +17,37 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+        $this->call(OrganizationSeeder::class);
+        $divisions = Division::all();
+        $divisionCount = $divisions->count();
 
-        $roles = ['admin','dispatcher','worker','supervisor','citizen'];
+        $roleCounts = [
+            'admin'=>5,
+            'dispatcher'=>5,
+            'worker'=>40,
+            'supervisor'=>10,
+            'citizen'=>5
+            ];
 
-        foreach($roles as $role){
-            for($i = 1;$i <= 5;$i++){
-                User::factory()->create([
+        foreach($roleCounts as $role => $count){
+            for($i = 1;$i <= $count;$i++){
+                $user = User::factory()->create([
                     'name' => ucfirst($role) . ' ' . $i,
                     'email' => $role . $i . '@gmail.com',
                     'role' => $role
                 ]);
+
+                if(in_array($role,['worker','supervisor'])){
+                    $assignedDivision = $divisions[$i % $divisionCount];
+
+                    Employee::create([
+                        'user_id' => $user->id,
+                        'division_id' => $assignedDivision->id,
+                        'job_title' => $role === 'supervisor' ? 'Division Supervisor' : 'Field Technician',
+                    ]);
+                }
             }
         }
-
-        $this->call(OrganizationSeeder::class);
+        $this->command->info('Users and Employees seeded successfully! (including 40 Workers & 10 Supervisors)');
     }
 }
