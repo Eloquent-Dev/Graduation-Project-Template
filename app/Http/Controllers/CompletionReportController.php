74c757972eaf7c6b5e1772067ae3bac38402d058
Ali Mohammad <aligamerhd15@gmail.com>
2026-03-27 21:59:01 +0300

    5.1.2 'CompletionReport index'

--- app/Http/Controllers/CompletionReportController.php
+++ app/Http/Controllers/CompletionReportController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\CompletionReport;
 
 class CompletionReportController extends Controller
 {
-    //
+    public function index (){
+        CompletionReport::with(['jobOrder.complaint.category'])
+        ->where('reported_by', auth()->id())
+        ->orderBy('created_at','desc')
+        ->paginate(15);
+
+        return view('supervisor.reports.index', compact('reports'));
+    }
 }
