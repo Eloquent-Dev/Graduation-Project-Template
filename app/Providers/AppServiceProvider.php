93de20426853d82edc3ddf33c011624852c5d500
Ali Mohammad <aligamerhd15@gmail.com>
2026-04-18 02:20:34 +0300

    8.19.5 'made ratelimiters code cleaner and added two limit lines for complaint submission by ip and user id'

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -37,53 +37,59 @@ class AppServiceProvider extends ServiceProvider
             SetEmployeeOffDutyOnLogout::class
         );
 
-        RateLimiter::for('login' , function(Request $request){
-            return Limit::perMinute(5)->by('ip_'.$request->ip)
-            ->response(function(){
-                return response(abort(429,'Too many login attempts.'));
-            });
+        RateLimiter::for('login', function (Request $request) {
+            return Limit::perMinute(5)->by('ip_' . $request->ip())
+                ->response(function () {
+                    abort(429, 'Too many login attempts.');
+                });
         });
 
-        RateLimiter::for('register', function(Request $request){
-            return Limit::perMinutes(30,10)->by('ip_'.$request->ip)
-            ->response(function(){
-                return response(abort(429, 'Too many registration attempts.'));
-            });
+        RateLimiter::for('register', function (Request $request) {
+            return Limit::perMinutes(30, 10)->by('ip_' . $request->ip())
+                ->response(function () {
+                    abort(429, 'Too many registration attempts.');
+                });
         });
 
-        RateLimiter::for('oauth', function(Request $request){
-            return Limit::perMinutes(30,10)->by('ip_'.$request->ip)
-            ->response(function(){
-                return response(abort(429, 'Too many oauth registration attempts.'));
-            });
+        RateLimiter::for('oauth', function (Request $request) {
+            return Limit::perMinutes(30, 10)->by('ip_' . $request->ip())
+                ->response(function () {
+                    abort(429, 'Too many oauth registration attempts.');
+                });
         });
 
-        RateLimiter::for('complaints' , function(Request $request){
-            return Limit::perMinute(3)->by('ip_'.$request->ip)
-            ->response(function(){
-                return response(abort(429,'Too many complaints submitted.'));
-            });
+        RateLimiter::for('complaints', function (Request $request) {
+            return [
+                Limit::perMinute(3)->by('ip_' . $request->ip())
+                    ->response(function () {
+                        abort(429, 'Too many complaints submitted.');
+                    }),
+                Limit::perMinute(3)->by('user_' . $request->user()->id)
+                    ->response(function () {
+                        abort(429, 'Too many complaints submitted.');
+                    })
+            ];
         });
 
-        RateLimiter::for('feedback' , function(Request $request){
-            return Limit::perMinute(3)->by('user_'.$request->user()->id)
-            ->response(function(){
-                return response(abort(429,'Too many complaint feedback submitted.'));
-            });
+        RateLimiter::for('feedback', function (Request $request) {
+            return Limit::perMinute(3)->by('user_' . $request->user()->id)
+                ->response(function () {
+                    abort(429, 'Too many complaint feedback submitted.');
+                });
         });
 
-        RateLimiter::for('password' , function(Request $request){
-            return Limit::perHour(3)->by('user_'.$request->user()->id)
-            ->response(function(){
-                return response(abort(429,'Too many password changes.'));
-            });
+        RateLimiter::for('password', function (Request $request) {
+            return Limit::perHour(3)->by('user_' . $request->user()->id)
+                ->response(function () {
+                    abort(429, 'Too many password changes.');
+                });
         });
 
-        RateLimiter::for('tracking-data' , function(Request $request){
-            return Limit::perMinute(15)->by('user_'.$request->user()->id)
-            ->response(function(){
-                return response(abort(429,'Too many tracking data requests.'));
-            });
+        RateLimiter::for('tracking-data', function (Request $request) {
+            return Limit::perMinute(15)->by('user_' . $request->user()->id)
+                ->response(function () {
+                    abort(429, 'Too many tracking data requests.');
+                });
         });
     }
 }
